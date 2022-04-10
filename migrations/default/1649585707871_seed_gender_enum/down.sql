61dce1c7f2673d13b1053a1e61d90a6cccfcd7fd
Nayan Das <dastms@gmail.com>
2022-04-10 16:19:56 +0530

    Fix gender enums and add gender seed migrations

---
+++ migrations/default/1649585707871_seed_gender_enum/down.sql
@@ -0,0 +1,6 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- INSERT INTO gender_enum VALUES
+-- ('MALE'),
+-- ('FEMALE'),
+-- ('OTHER');
---
+++ migrations/default/1649585707871_seed_gender_enum/up.sql
@@ -0,0 +1,4 @@
+INSERT INTO gender_enum VALUES 
+('MALE'),
+('FEMALE'),
+('OTHER');
